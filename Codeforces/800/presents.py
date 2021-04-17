8724d35e145e00c5d3f8b1bf6e7e6be10e4d6ddc
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2021-04-17 12:13:41 -0500

    presents python 1st solution

--- Codeforces/800/presents.py
+++ Codeforces/800/presents.py
@@ -1,11 +1,11 @@
 def main():
     n = int(input())
-    l = input().split()
-    l = [int(x) for x in l]
+    gift_list = input().split()
+    gift_list = [int(x) for x in gift_list]
     result = [0]*n
 
     for i in range(n):
-        result[l[i]-1] = i+1
+        result[gift_list[i]-1] = i+1
 
     print(' '.join(str(x) for x in result))
 
