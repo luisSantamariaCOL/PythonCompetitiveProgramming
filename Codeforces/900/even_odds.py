92c150e088cb09a512e444db9b8725d5ec2908a7
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2021-04-21 16:43:55 -0500

    even odds Codeforces python solution

---
+++ Codeforces/900/even_odds.py
@@ -0,0 +1,10 @@
+def main():
+    n, k = map(int, input().split())
+    odds, evens = (n + 1) // 2, n // 2
+    if k <= odds:
+        print(int(k * 2) - 1)
+    else:
+        print(int((k - odds) * 2))
+
+if __name__ == '__main__':
+    main()
