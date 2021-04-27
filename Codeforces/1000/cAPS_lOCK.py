fa238396935fb7e7286ca3b67d256d4bf1ae8737
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2021-04-27 11:32:47 -0500

    cAPS lOCK codeforces python solution

--- Codeforces/1000/cAPS_lOCK.py
+++ Codeforces/1000/cAPS_lOCK.py
@@ -1,14 +1,12 @@
 def main():
     word = input()
 
-    # 1 len cases
     if len(word) == 1:
         if word.isupper():
             print(word.lower())
         else:
             print(word.upper())
     else:
-        # cap case
         if word.isupper():
             print(word.lower())
         elif word[0].islower() and word[1:].isupper():
