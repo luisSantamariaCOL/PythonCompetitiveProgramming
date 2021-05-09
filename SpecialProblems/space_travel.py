41bda827ba774593e801bbe4bc46c921da5501f4
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2021-05-08 21:54:06 -0500

    Space Travel Perficient Problem python template

---
+++ SpecialProblems/space_travel.py
@@ -0,0 +1,24 @@
+'''
+Space Traveling Problem
+by luisSantamariaCOL
+
+paths:
+["Mars Saturn"]
+["Sun Earth", "Mercury Venus", "Mars Mercury", "Mercury Saturn"]
+
+posible paths:
+Mars Saturn
+Mars Mercury Venus
+Mars Mercury Saturn
+Sun Earth
+
+Expected output:
+Mars Mercury Venus
+Sun Earth
+'''
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
