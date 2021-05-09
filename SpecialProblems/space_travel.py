d98d80e9b8ab871b6e997bb0c2e876c9b930e479
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2021-05-08 22:12:57 -0500

    Input done | Perficient Problem | python 3

--- SpecialProblems/space_travel.py
+++ SpecialProblems/space_travel.py
@@ -18,7 +18,16 @@ Sun Earth
 '''
 
 def main():
-    pass
+    paths = {}
+    n = int(input())
+    for i in range(n):
+        first_p, second_p = input().split()
+        if first_p not in paths:
+            paths[first_p] = [second_p]
+        else:
+            paths.get(first_p).append(second_p)
+
+    print(paths)
 
 if __name__ == '__main__':
     main()
