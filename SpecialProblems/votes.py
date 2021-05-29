27f5bd11d6ea8bdd0ea4dcee3cd46b481edaa957
LuisSantamariaCOL <luisestebansantamaria@outlook.com>
2021-05-29 17:13:56 -0500

    vote elections special problem dictionary v1

--- SpecialProblems/votes.py
+++ SpecialProblems/votes.py
@@ -4,25 +4,8 @@
 # nine -> 9
 # thirtythree -> 22
 
-def vote_validation(str_vote : str):
-    if str_vote.isalpha():
-        return 1
-    else:
-        return 0
-
-
-def convert_vote(str_vote):
-    if not vote_validation(str_vote):
-        return 0
-    return 1
-
-def voting(total_count):
-    vote = input("Enter vote: ")
-    count = convert_vote(vote)
-    total_count += count
-
-if __name__ == '__main__':
-    conversion = {'one': 1,
+def main():
+    numbers = {'one': 1,
                   'two': 2,
                   'three': 3,
                   'four': 4,
@@ -44,9 +27,15 @@ if __name__ == '__main__':
                   'eighty': 80,
                   'ninety': 90}
     total_count = 0
+
     while True:
-        voting(total_count)
-        print("Enter '0' or 'exit' to exit")
-        choice = input()
-        if choice == '0' or choice.lower() == 'exit':
-            break
+        vote = input("Enter vote: ")
+        if not vote.isalpha():
+            print("Not a valid vote")
+        else:
+            if vote in numbers:
+                total_count += numbers[vote]
+        print("Total count:", total_count)
+
+if __name__ == '__main__':
+    main()
