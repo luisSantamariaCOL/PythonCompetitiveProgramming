aa10260e2c324b79ab583e3ad1cb9daba9e57dbf
LuisSantamariaCOL <luisestebansantamaria@outlook.com>
2021-06-06 11:52:14 -0500

    Cheap travel problem | Codeforces | Python3 solution

--- Codeforces/1200/cheap_travel.py
+++ Codeforces/1200/cheap_travel.py
@@ -1,6 +1,11 @@
 def main():
-    pass
-
+    n, m, a, b = list(map(int ,input().split()))
+    minimum_money_to_spend = 0
+    if a*m < b:
+        minimum_money_to_spend = a*n
+    else:
+        minimum_money_to_spend = (n//m)*b + (n%m)*a
+    print(minimum_money_to_spend)
 
 if __name__ == '__main__':
     main()
