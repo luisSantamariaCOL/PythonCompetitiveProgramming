f1c67608196bac182310fefc36984c61bdc12373
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2021-07-10 11:15:44 -0500

    25A - IQ test | Codeforces | Python3 solution

--- Codeforces/1300/iq_test.py
+++ Codeforces/1300/iq_test.py
@@ -1,30 +1,13 @@
 def main():
-    n = int(input())
-    numbers = list(map(int, input().split()))
+    n = input()
+    numbers = [int(x) for x in input().split()]
 
-    evens = [x for x in range(101) if x % 2 == 0]
-    odds = [x for x in range(100) if x % 2 == 1]
+    is_evens = [x % 2 for x in numbers]
 
-    if len(set(numbers)) == 2:
-        unique = [x for x in numbers if numbers.count(x) == 1]
-        i = numbers.index(unique[0])
-        print(i + 1)
-        return
-
-    evens_in_list = []
-    odds_in_list = []
-    for i in range(n):
-        if numbers[i] in evens:
-            evens_in_list.append(numbers[i])
-        else:
-            odds_in_list.append(numbers[i])
-    if len(evens_in_list) == 1:
-        i = numbers.index(evens_in_list[0])
-        print(i+1)
-    elif len(odds_in_list) == 1:
-        i = numbers.index(odds_in_list[0])
-        print(i + 1)
-
+    if is_evens.count(0) > is_evens.count(1):
+        print(is_evens.index(1) + 1)
+    else:
+        print(is_evens.index(0) + 1)
 
 if __name__ == '__main__':
     main()
